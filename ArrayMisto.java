13dba9bd8efe52f6518a1e7d2c697129a7dd43d4
WagnerRamos1 <wagner.ramos@academico.ifpb.edu.br>
2025-12-11 10:56:09 -0300

    Delete ArrayMisto.java

--- ArrayMisto.java
+++
@@ -1,9 +0,0 @@
-public class ArrayMisto {
-    Vertice vertice;
-    Integer distancia;
-
-    public ArrayMisto(Vertice vertice, Integer distancia) {
-        this.vertice = vertice;
-        this.distancia = distancia;
-    }
-}
